50f5bc05444e1d1c6f553d10914179c86d4dd6a6
Adam O'Neil <adamosoftware@gmail.com>
2023-12-20 18:28:49 -0500

    Add project files.

---
+++ JewelRun.Abstractions/Player.cs
@@ -0,0 +1,26 @@
+﻿namespace JewelRun.Abstractions;
+
+public class Player
+{
+    private Player()
+    {            
+    }
+
+    public static Player Create(string name)
+    {
+        var runners = Runner.Names.Select(name => new { Name = name, Runner = Runner.Create() });
+       
+        return new Player()
+        { 
+            Name = name,
+            Runners = runners.Select(r => r.Runner).ToArray(),
+            RunnersByName = runners.ToDictionary(r => r.Name, r => r.Runner),
+            Jeweler = runners.MaxBy(r => r.Runner.Score)!.Name 
+        };
+    }
+
+    public required string Name { get; init; } = default!;
+	public required Runner[] Runners { get; init; } = Array.Empty<Runner>();
+	public Dictionary<string, Runner> RunnersByName { get; private set; } = new();
+	public required string Jeweler { get; init; } = default!;
+}
---
+++ JewelRun.Abstractions/Runner.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+
+namespace JewelRun.Abstractions;
+
+public class Runner
+{
+    private Runner()
+    {			
+    }
+
+	public static Runner Create() => new()
+	{
+		Awareness = GetRandom(1, 4),
+		Attack = GetRandom(1, 3),
+		Defense = GetRandom(1, 3),
+		Mobility = GetRandom(3, 7)
+	};
+
+	private static int GetRandom(int min, int max) => RandomNumberGenerator.GetInt32(min, max);
+
+    public int Awareness { get; init; }
+	public int Attack { get; init; }
+	public int Defense { get; init; }
+	public int Mobility { get; init; }
+	public int Score => Awareness + Attack + Defense + Mobility;
+
+	public void SetIsAlive(bool isAlive) { IsAlive = isAlive; }
+	public void SetIsVisible(bool isVisible) { IsVisible = isVisible; }
+
+	public bool IsAlive { get; private set; }
+	public bool IsVisible { get; private set; }	
+
+	public static string[] Names =>
+	[
+		"Apple",
+		"Banjo",
+		"Comet",
+		"Doodle",
+		"Ember",
+		"Fancy",
+		"Garlic"
+	];
+}
