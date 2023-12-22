4f3f46f22b1009b8924da0b4ccddb593ed358f92
Adam O'Neil <adamosoftware@gmail.com>
2023-12-21 19:01:46 -0500

    basics of game grid

---
+++ GameBoard.Abstractions/GameGrid.cs
@@ -0,0 +1,13 @@
+﻿namespace GameBoard.Abstractions;
+
+public class GameGrid
+{
+    public GameGrid()
+    {            
+    }
+
+    public static GameGrid Create(int width,  int height) => new() { Width = width, Height = height };
+
+    public int Width { get; init; }
+	public int Height { get; init; }
+}
---
+++ GameBoard.App/GameBoard.App/Extensions/GameGridExtensions.cs
@@ -0,0 +1,31 @@
+﻿using GameBoard.Abstractions;
+using Microsoft.AspNetCore.Components;
+using System.Text;
+
+namespace GameBoard.App.Extensions;
+
+public static class GameGridExtensions
+{
+	public static MarkupString Css(this GameGrid gameGrid) =>
+		new(@$"border: 1px gray solid;
+			width:93vw;
+			height:93vh;
+			grid-template-rows:repeat({gameGrid.Height}, 1fr);
+			grid-template-columns:repeat({gameGrid.Width}, 1fr);");
+
+	public static MarkupString Body(this GameGrid gameGrid)	
+	{
+		StringBuilder sb = new();
+
+		for (int x = 1; x <= gameGrid.Width; x++)
+		{
+			for (int y = 1;  y <= gameGrid.Height; y++)
+			{
+				sb.Append($"<div id=\"square-{x}-{y}\" class=\"gridCell\">{x}, {y}</div>");
+			}
+		} 
+
+		return new(sb.ToString());
+	}
+		
+}
